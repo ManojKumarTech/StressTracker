bb6a52ea3bccd16de6b01b8a9c6f909db9664c80
ManojKumar <mano22056.it@rmkec.ac.in>
2024-09-23 12:40:03 +0530

    Test page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Login from './pages/login';
-import Testing from './pages/Testing';
+import Testing from './pages/Test/Testing';
+import QuestionsPage from './pages/Test/QuestionsPage';
+import ResultPage from './pages/Test/ResultPage';
 
 
 function App() {
@@ -14,7 +16,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Login/>} />
       <Route  path="/home" element={<Home/>} />
-      <Route path='/test' element={<Testing/>}/>
+      <Route path="/testing" element={<Testing />} />
+      <Route path="/questions/:ageGroup" element={<QuestionsPage />} />
+      <Route path='/result' element={<ResultPage/>}/>
     </Routes>
     </BrowserRouter>
   )
