a0af00e4804ebcd45a6cd56b1e58799c4b99e184
ManojKumar <mano22056.it@rmkec.ac.in>
2025-01-23 22:12:34 +0530

    Final Commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -55,58 +55,8 @@ function Footer() {
                                 </p>
                             </div>
                             {/* Grid column */}
-                            {/* Grid column */}
-                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                                {/* Links */}
-                                <h6 className="text-uppercase fw-bold mb-4">Services</h6>
-                                <p>
-                                    <a href="#!" className="text-reset">
-                                        Stress Management
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">
-                                        Mindfulness Resources
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">
-                                        Daily Mood Tracking
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">
-                                        Guided Meditation
-                                    </a>
-                                </p>
-                            </div>
-                            {/* Grid column */}
-                            {/* Grid column */}
-                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                                {/* Links */}
-                                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-                                <p>
-                                    <a href="#!" className="text-reset">
-                                        Pricing
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">
-                                        Settings
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">
-                                        My Progress
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">
-                                        Help Center
-                                    </a>
-                                </p>
-                            </div>
-                            {/* Grid column */}
+                            
+                            
                             {/* Grid column */}
                             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                                 {/* Links */}
